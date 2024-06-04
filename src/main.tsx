b01af938c3b3eeffd3fb4bd319a51ca6c7d22169
Nobu <chat@nobu.sh>
2024-06-03 20:06:45 -0500

    terms

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import { Main } from "./site";
+import { Terms } from "./terms";
 
 import "./index.scss";
-import { Main } from "./site";
+
+const router = createBrowserRouter([
+	{
+		path: "/terms",
+		Component: () => <Terms />
+	},
+	{
+		path: "*",
+		Component: () => <Main />
+	}
+]);
 
 ReactDOM.createRoot(document.querySelector("#root")!).render(
 	<React.StrictMode>
-		<Main />
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -57,5 +57,9 @@ export default {
 			}
 		}
 	},
-	plugins: [require("tailwindcss-animate"), require("tailwind-scrollbar")]
+	plugins: [
+		require("tailwindcss-animate"),
+		require("tailwind-scrollbar"),
+		require("@tailwindcss/typography")
+	]
 };
